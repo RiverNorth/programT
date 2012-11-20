64f673d380489f4f4f02b3727b24f487b37d7eaa
unknown <kv@.(none)>
2012-11-21 00:30:13 +0800

    1121home

--- memorypool/Event.cpp
+++ memorypool/Event.cpp
@@ -1,5 +1,7 @@
 #include "Event.h"
 #include "Memory_Pool.h"
+#include <stdexcept>
+#include <iostream>
 
 Memory_Pool<Event> temp;
 Memory_Pool<Event> Event::pool=temp;
@@ -8,9 +10,13 @@ int main(int argc,char **argv)
 	int i=0;
 	while(i<1000)
 	{
-		Event *e=new(std::nothrow)Event;
+		try{
+		Event *e=new Event();
 		if(e==0)
 			break;
+		}catch(const std::bad_alloc &e){
+			std::cout<<"allocate error"<<std::endl;
+		}
 	}
 	return 0;
 }
--- memorypool/Memory_Pool.h
+++ memorypool/Memory_Pool.h
@@ -5,7 +5,8 @@ extern "C"{
 }
 #include "stdio.h"
 #include <iostream>
-
+#include <stdexcept>
+#include "Memory_Pool_Base.h"
 
 struct Point
 {
@@ -13,13 +14,50 @@ struct Point
 	void *pointer;
 };
 
-template<class Type> class Memory_Pool
+template<class Type> class Memory_Pool:public Memory_Pool_Base
 {
 	typedef struct Point Point;
 	public:
-		void* allocate();
-		void deallocate(void *p);
-		Memory_Pool<Type>(int init_size_t=5,int max_size_t=20);
+		virtual void* allocate()
+		{
+			#ifdef DEBUG
+			allco_num++;
+			std::cout<<allco_num<<std::endl;
+			#endif
+			if(head==0||head==end)
+				{
+					throw std::bad_alloc();
+				}
+			temp=head;
+			head=head->next;
+			return temp->pointer;
+		};
+		virtual void deallocate(void *p)
+		{
+			if(end->next=0)
+			std::cout<<"error when dellocate"<<std::endl;
+			end=end->next;
+			end->pointer=p;
+		};
+		Memory_Pool<Type>(int init_size_t=5,int max_size_t=20)
+		{
+			#ifdef DEBUG
+			allco_num=0;
+			#endif
+			temp=0;
+			beginMemory=(Type*)malloc(init_size*sizeof(Type));
+			endMemory=beginMemory+init_size_t-1;
+			head=(Point*)malloc(sizeof(Point));
+			head->pointer=beginMemory;
+			end=head;
+			for(int i=0;i<init_size-1;i++)
+			{
+				end->next=(Point*)malloc(sizeof(Point));
+				end=end->next;
+				end->pointer=beginMemory+i+1;
+			}
+			end->next=0;
+		};
 	private:
 		int init_size;
 		int max_size;
@@ -47,51 +85,5 @@ template<class Type> class Memory_Pool
 		// 	}
 		// 	end->next=NULL;
 		// }
-};
-
-
-
-template<class Type> void* Memory_Pool<Type>::allocate()
-{
-	#ifdef DEBUG
-	allco_num++;
-	std::cout<<allco_num<<std::endl;
-	#endif
-	if(head==0||head==end)
-		{
-			return 0;
-		}
-	temp=head;
-	head=head->next;
-	return temp->pointer;
-};
-
-template<class Type> Memory_Pool<Type>::Memory_Pool(int init_size_t,int max_size_t):
-	init_size(init_size_t),max_size(5),used_size(0)
-{
-	#ifdef DEBUG
-	allco_num=0;
-	#endif
-	temp=0;
-	beginMemory=(Type*)malloc(init_size*sizeof(Type));
-	endMemory=beginMemory+init_size_t-1;
-	head=(Point*)malloc(sizeof(Point));
-	head->pointer=beginMemory;
-	end=head;
-	for(int i=0;i<init_size-1;i++)
-	{
-		end->next=(Point*)malloc(sizeof(Point));
-		end=end->next;
-		end->pointer=beginMemory+i+1;
-	}
-	end->next=0;
-};
-
-template<class Type> void Memory_Pool<Type>::deallocate(void *p)
-{
-	if(end->next=0)
-		std::cout<<"error when dellocate"<<std::endl;
-	end=end->next;
-	end->pointer=p;
 };
 #endif
